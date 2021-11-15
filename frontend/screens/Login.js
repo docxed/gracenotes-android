91134e7578ed2e0b94754501c0403be4469852c2
EveGalaxy <evez.07@hotmail.com>
2021-11-15 21:03:05 +0700

    แก้ input หน้า Login&Regis

--- frontend/screens/Login.js
+++ frontend/screens/Login.js
@@ -84,7 +84,7 @@ function Login_Screen({ navigation }) {
         >
           ลงชื่อเข้าใช้
         </Heading>
-        <VStack space={4} mt="5" borderRadius={5} padding={15} shadow={4}>
+        <VStack space={4} mt="5" padding={15}>
           <FormControl>
             <FormControl.Label
               _text={{
